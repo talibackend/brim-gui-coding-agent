2a72298322dfb16581abfb578230f4b2db64359d
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2026-05-09 23:27:02 +0100

    Application looks more serverless

--- mastra/src/mastra/index.ts
+++ mastra/src/mastra/index.ts
@@ -3,8 +3,6 @@ import { PinoLogger } from '@mastra/loggers';
 import { mainAgent } from './agents/main.agent';
 import { fileContentAgent } from './agents/filecontent.agent';
 import { hierarchicalGraphAgent } from './agents/hierarchicalGraph.agent';
-import { Store } from '../config/store.config';
-import { VectorStore } from '../config/vector.config';
 import { codebaseGraphGeneratorWorkflow } from './workflows/codebaseGraphGenerator.workflow';
 import { exclusionPatternsAgent } from './agents/exclusionPatterns.agent';
 
